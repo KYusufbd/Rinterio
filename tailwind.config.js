329d5c1181b9b233563d3149fc4b6bab68a1699c
Kazi Yusuf <yfaka001@gmail.com>
2024-08-27 00:29:36 +0600

    Created responsive navbar. Now time to make some corrections

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,18 @@ module.exports = {
     fontFamily: {
       'manrope': ['Manrope'],
     },
+    fontSize: {
+      base: ['16px', '24px'],
+      "3xl": "32px",
+    },
     extend: {
       colors: {
+        "primary": '#abef5f',
+        "primary-50": '#abef5f80',
         "txt-color-1": '#212121',
         "txt-color-2": '#494949',
+        "txt-color-3": '#737373',
+        "txt-color-4": '#1C1C1C',
         "body-bg": '#F4F4F4',
       }
     },
